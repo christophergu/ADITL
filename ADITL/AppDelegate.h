208376b4e952492141ef652f47bca6189d0c0bd6
Christopher Gu <christopher.gu@gmail.com>
2014-05-14 15:23:39 -0500

    Initial Commit

---
+++ ADITL/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ADITL
+//
+//  Created by Christopher Gu on 5/14/14.
+//  Copyright (c) 2014 Christopher Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ADITL/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ADITL
+//
+//  Created by Christopher Gu on 5/14/14.
+//  Copyright (c) 2014 Christopher Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
