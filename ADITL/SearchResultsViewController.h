a685b8fb580a833d9eceffb144b817d39baa7c7a
Christopher Gu <christopher.gu@gmail.com>
2014-05-19 14:49:00 -0500

    started on the search results vc

---
+++ ADITL/SearchResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchResultsViewController.h
+//  ADITL
+//
+//  Created by Christopher Gu on 5/19/14.
+//  Copyright (c) 2014 Christopher Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchResultsViewController : UIViewController
+
+@end
---
+++ ADITL/SearchResultsViewController.m
@@ -0,0 +1,23 @@
+//
+//  SearchResultsViewController.m
+//  ADITL
+//
+//  Created by Christopher Gu on 5/19/14.
+//  Copyright (c) 2014 Christopher Gu. All rights reserved.
+//
+
+#import "SearchResultsViewController.h"
+
+@interface SearchResultsViewController ()
+
+@end
+
+@implementation SearchResultsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
